0a29224a730fedfa626787d3bbabf3d0fd8d03ab
Alex Oliveira <alexoliveira7x@gmail.com>
2024-01-30 03:38:50 +0000

    chore: update description

--- orbit/apps/orbit-app/src/pages/user/index.tsx
+++ orbit/apps/orbit-app/src/pages/user/index.tsx
@@ -2,11 +2,17 @@ import { Container } from "@mantine/core";
 import { useTranslation } from "@orbit/translations";
 
 export default function User() {
+  // This will be loaded
   const { t } = useTranslation('user');
+  const { t: ttt } = useTranslation('validations');
+  // This will not be loaded
+  const { t: tt } = useTranslation('global');
 
 return (
   <Container>
     <h1>{t('hello_user')}</h1>
+    <h1>{tt('hello_user')}</h1>
+    <h1>{ttt('hello_there')}</h1>
   </Container>
 )
 }
